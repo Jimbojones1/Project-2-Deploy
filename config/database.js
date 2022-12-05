48b4e092ceff626ba4effe1027b6e11301e8c1f6
JimHaff <james.haff@generalassemb.ly>
2022-12-05 17:05:24 -0600

    updated db connection

--- config/database.js
+++ config/database.js
@@ -3,34 +3,27 @@ const mongoose = require("mongoose");
 // /movies, connect to a movies database, or create a movies database
 
 module.exports = {
-  connectDB
-}
+  connectDB,
+};
 
-async function connectDB(){
+async function connectDB() {
   try {
-    mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-      });
+    const conn = await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-      // shortcut to mongoose.connection object
-const db = mongoose.connection;
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
 
-db.on("connected", function () {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
-});
+    // shortcut to mongoose.connection object
 
-db.on("error", function (err) {
-  console.log(`There was an ${err}`);
-});
-  } catch(err){
-    console.log('err');
-    process.exit(1)
+    console.log("connnnnnnnneected");
+  } catch (err) {
+    console.log("err");
+    console.log(err, ' connecting to mongodb')
+    process.exit(1);
   }
 }
-
-
-
 
 /// THE PURPOSE OF THIS FILE IS TO CREATE/CONNECT to a database, and establish a connection,
 // between our Express Server and Mongodb (our database server)
