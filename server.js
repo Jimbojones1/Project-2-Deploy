883102555626eee7f3dc87b59cad8dc83ff90acc
JimHaff <james.haff@generalassemb.ly>
2022-12-13 14:08:09 -0600

    Added no cache

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const methodOverride = require('method-override');
 const MongoStore = require('connect-mongo');
-
+const nocache = require("nocache");
 
 const indexRouter = require('./routes/index');
 const moviesRouter = require('./routes/movies');
@@ -24,11 +24,11 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(nocache());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(methodOverride('_method'));
 
 
