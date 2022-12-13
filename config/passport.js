e3190b1236d4f3a302ea7f16188928f2fc93a1a2
JimHaff <james.haff@generalassemb.ly>
2022-12-13 17:43:20 -0600

    changed strategy

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const User = require('../models/user');
 //Implement our strategy to authorize with google oauth
 
